ea472e5795525b6f329abcfd9c5a352549a2e17c
ahlem25 <ahlem.cherni112@gmail.com>
2025-03-09 14:02:51 +0100

    correction des divers

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,7 @@
-
+<!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('events.index') }}" class="nav-link {{ Request::is('events*') ? 'active' : '' }}">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
-        <p>Events</p>
+        <p>Home</p>
     </a>
 </li>
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::resource('events', App\Http\Controllers\EventController::class);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
