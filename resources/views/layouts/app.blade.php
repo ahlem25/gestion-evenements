cbaf4b5a6d4ccbd28651e8e65d2d425222ca6057
Ahlem cherni <ahlem.cherni112@gmail.com>
2025-03-09 14:16:36 +0100

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,13 @@
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
                             <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
                                 class="user-image img-circle elevation-2" alt="User Image">
-                            <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
+                            <span class="d-none d-md-inline">
+                                @if (Auth::check())
+                                    {{ Auth::user()->name }}
+                                @else
+
+                                @endif
+                            </span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                             <!-- User image -->
@@ -25,8 +31,18 @@
                                 <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
                                     class="img-circle elevation-2" alt="User Image">
                                 <p>
-                                    {{ Auth::user()->name }}
-                                    <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
+                                    @if (Auth::check())
+                                        {{ Auth::user()->name }}
+                                    @else
+
+                                    @endif
+                                    <small>
+                                        @if (Auth::check())
+                                            Member since {{ Auth::user()->created_at->format('M. Y') }}
+                                        @else
+
+                                        @endif
+                                    </small>
                                 </p>
                             </li>
                             <!-- Menu Footer-->
